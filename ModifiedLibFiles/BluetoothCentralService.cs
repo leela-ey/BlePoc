a7e012c46018eab11d0ad9e272611881ff619b29
Leela <leeladevi.balasundaram@ey.com>
2020-07-02 19:21:04 -0400

    Changes introduced in EY.Mobile.Bluetooth
    These files are modified in EY.Mobile.Bluetooth implemetnation of iOS

---
+++ ModifiedLibFiles/BluetoothCentralService.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using CoreBluetooth;
+using CoreFoundation;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+using Foundation;
+using EY.Mobile.Bluetooth.EventArgs;
+using System.Collections.Generic;
+
+namespace EY.Mobile.Bluetooth.iOS
+{
+    /// <summary>
+    ///     BluetoothCentralService class contains methods to create BLE central, scan/stopScan for peripherals, handle discovered/connected/disconnected peripherals
+    /// </summary>
+    public class BluetoothCentralService : CBCentralManagerDelegate, IBluetoothCentralService
+    {
+        private static string _restorationIdentifier;
+        private static bool _showPowerAlert = true;
+
+        CBCentralManager centralManager;
+
+        public static void UseRestorationIdentifier(string restorationIdentifier)
+        {
+            _restorationIdentifier = restorationIdentifier;
+        }
+
+        public static void ShowPowerAlert(bool showPowerAlert)
+        {
+            _showPowerAlert = showPowerAlert;
+        }
+
+        public BluetoothCentralService()
+        {
+            Console.WriteLine("iOS :: Concstructor of BluetoothCentralService invoked");
+        }
+
+        #region 1. Creation of Ble Central object
+
+        // Create Bluetooth Low Energy central object
+        public void IntializeBluetoothCentralService()
+        {
+            // prepare init options
+            CBCentralInitOptions initOptions = CreateInitOptions();
+
+            //centralManagerDelegate = new BleCentralManagerDelegate();
+
+            // create ble central object
+            centralManager = new CBCentralManager(this, DispatchQueue.CurrentQueue, initOptions);
+        }
+
+        private CBCentralInitOptions CreateInitOptions()
+        {
+            return new CBCentralInitOptions
+            {
+                RestoreIdentifier = _restorationIdentifier,
+                ShowPowerAlert = _showPowerAlert
+            };
+        }
+
+        public bool IsScanning()
+        {
+            return (centralManager != null)? centralManager.IsScanning : false;
+        }
+
+        public Task StartScanningForDevicesAsync(ScanMode mode, Guid[] serviceUuids = null, bool allowDuplicatesKey = false, CancellationToken cancellationToken = default)
+        {
+            if (centralManager.State == CBCentralManagerState.PoweredOn)
+            {
+                Console.WriteLine("BluetoothCentralService: Starting a scan for devices.");
+
+                CBUUID[] serviceCbuuids = null;
+                if (serviceUuids != null && serviceUuids.Any())
+                {
+                    serviceCbuuids = serviceUuids.Select(uuid => uuid.cbuuidFromGuid()).ToArray();
+                }
+
+                centralManager.ScanForPeripherals(serviceCbuuids, new PeripheralScanningOptions { AllowDuplicatesKey = allowDuplicatesKey });
+                Console.WriteLine("BluetoothCentralService: Scanning for " + serviceCbuuids.First());
+                return Task.CompletedTask;
+            }
+            else
+            {
+                throw new TaskCanceledException("Scanning cancelled, Bluetooth is not ON");
+            }
+        }
+
+        public Task StopScanningForDevicesAsync()
+        {
+            centralManager.StopScan();
+            return Task.CompletedTask;
+        }
+
+        public Task ConnectToDeviceAsync(IBTDevice device, CancellationToken cancellationToken = default)
+        {
+            if (device != null)
+            {
+                centralManager.ConnectPeripheral(device.NativeDevice as CBPeripheral, new PeripheralConnectionOptions());
+
+                cancellationToken.Register(() =>
+                {
+                    Console.WriteLine("Canceling the connect attempt");
+                    centralManager.CancelPeripheralConnection(device.NativeDevice as CBPeripheral);
+                });
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task DisconnectDeviceAsync(IBTDevice device)
+        {
+            centralManager.CancelPeripheralConnection(device.NativeDevice as CBPeripheral);
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+
+        #region 2. IBluetoothCentralService events implementation
+
+        private event EventHandler<BTDeviceEventArgs> _discoveredPeripheral;
+        public event EventHandler<BTDeviceEventArgs> DeviceDiscovered
+        {
+            add => _discoveredPeripheral += value;
+            remove => _discoveredPeripheral -= value;
+        }
+
+        private event EventHandler<BTDeviceEventArgs> _deviceConnected;
+        public event EventHandler<BTDeviceEventArgs> DeviceConnected
+        {
+            add => _deviceConnected += value;
+            remove => _deviceConnected -= value;
+        }
+
+        private event EventHandler<BTDeviceEventArgs> _deviceDisconnected;
+        public event EventHandler<BTDeviceEventArgs> DeviceDisconnected
+        {
+            add => _deviceDisconnected += value;
+            remove => _deviceDisconnected -= value;
+        }
+
+        private event EventHandler<BTDeviceErrorEventArgs> _deviceConnectionLost;
+        public event EventHandler<BTDeviceErrorEventArgs> DeviceConnectionLost
+        {
+            add => _deviceConnectionLost += value;
+            remove => _deviceConnectionLost -= value;
+        }
+
+
+        private event EventHandler<BTCentralStateRestoredEventArgs> _centralStateRestorediOS;
+        public event EventHandler<BTCentralStateRestoredEventArgs> CentralStateRestorediOS
+        {
+            add => _centralStateRestorediOS += value;
+            remove => _centralStateRestorediOS -= value;
+        }
+
+
+        #endregion
+
+        #region CBCentralManagerDelegate methods implementation
+
+        public override void UpdatedState(CBCentralManager central)
+        {
+            
+        }
+
+        public override void WillRestoreState(CBCentralManager central, NSDictionary dict)
+        {
+            BTCentralStateRestoredEventArgs args = new BTCentralStateRestoredEventArgs();
+
+            if (dict.ContainsKey(CBCentralManager.RestoredStatePeripheralsKey))
+            {
+                var value = dict[CBCentralManager.RestoredStatePeripheralsKey];
+                IEnumerable<CBPeripheral> restorePeripherals = value as IEnumerable<CBPeripheral>;
+                if (restorePeripherals != null && restorePeripherals.Count() > 0)
+                {
+                    IList<IBTDevice> restoredBtDevices = new List<IBTDevice>();
+                    foreach (var peripheral in restorePeripherals)
+                    {
+                        restoredBtDevices.Append(new BTDevice(peripheral, this));
+                    }
+                    args.AreBtDevicesToBeRestored = true;
+                    args.RestoredBtDevices = restoredBtDevices;
+                }
+            }
+
+            if (dict.ContainsKey(CBCentralManager.RestoredStateScanServicesKey))
+            {
+                var value = dict[CBCentralManager.RestoredStateScanServicesKey];
+                IEnumerable<CBUUID> restoredScanCBUuids = value as IEnumerable<CBUUID>;
+                if (restoredScanCBUuids != null && restoredScanCBUuids.Count() > 0)
+                {
+                    IList<Guid> restoredScanGuids = new List<Guid>();
+                    foreach (var cbuuid in restoredScanCBUuids)
+                    {
+                        restoredScanGuids.Append(cbuuid.GuidFromUuid());
+                    }
+                    args.IsScanningToBeRestored = true;
+                    args.RestoredScanServices = restoredScanGuids;
+                }
+            }
+
+            _centralStateRestorediOS?.Invoke(this, args);
+        }
+
+        public override void DiscoveredPeripheral(CBCentralManager central, CBPeripheral peripheral, NSDictionary advertisementData, NSNumber RSSI)
+        {
+            Console.WriteLine($"DiscoveredPeripheral: {peripheral.Name}, Id: {peripheral.Identifier}");
+
+            // create a new BTDevice object from discovered peripheral
+            var advRecord = ParseAdvertismentData(advertisementData);
+
+            var name = peripheral.Name;
+            if (advertisementData.ContainsKey(CBAdvertisement.DataLocalNameKey))
+            {
+                name = ((NSString)advertisementData.ValueForKey(CBAdvertisement.DataLocalNameKey)).ToString();
+            }
+            IBTDevice btDevice = new BTDevice(name, RSSI.Int32Value, peripheral, advRecord, this);
+
+            _discoveredPeripheral?.Invoke(this, new BTDeviceEventArgs() { Device = btDevice });
+        }
+
+        public override void ConnectedPeripheral(CBCentralManager central, CBPeripheral peripheral)
+        {
+            Console.WriteLine($"ConnectedPeripherial: {peripheral.Name}");
+
+            // when a peripheral gets connected, add that peripheral to our running list of connected peripherals
+            var guid = ParseDeviceGuid(peripheral).ToString();
+
+            IBTDevice btDevice = new BTDevice(peripheral, this);
+
+            _deviceConnected?.Invoke(this, new BTDeviceEventArgs { Device = btDevice });
+        }
+
+        public override void DisconnectedPeripheral(CBCentralManager central, CBPeripheral peripheral, NSError error)
+        {
+            if (error != null)
+            {
+                Console.WriteLine($"Disconnect error {error.Code} {error.Description} {error.Domain}");
+            }
+
+            IBTDevice disconnectedDevice = new BTDevice(peripheral, this);
+
+            // check if it is a peripheral disconnection, which would be treated as normal
+            if (error != null && error.Code == 7 && error.Domain == "CBErrorDomain")
+            {
+                Console.WriteLine($"DisconnectedPeripheral by user: {disconnectedDevice.Name}");
+                _deviceDisconnected?.Invoke(this, new BTDeviceEventArgs { Device = disconnectedDevice });
+            }
+            else
+            {
+                Console.WriteLine($"DisconnectedPeripheral by lost signal: {disconnectedDevice.Name}");
+                _deviceConnectionLost?.Invoke(this, new BTDeviceErrorEventArgs { Device = disconnectedDevice, ErrorMessage = error.LocalizedDescription });
+            }
+        }
+
+        #endregion
+
+        #region Parsers
+        // 1. Parse advertisement data to BTAdvertiseData
+        private AdvertisementRecord ParseAdvertismentData(NSDictionary advertisementData)
+        {
+            var advertisementRecord = new AdvertisementRecord();
+
+            foreach (var o in advertisementData.Keys)
+            {
+                var key = (NSString)o;
+                if (key == CBAdvertisement.DataLocalNameKey)
+                {
+                    advertisementRecord.CompleteLocalName =
+                        NSData.FromString(advertisementData.ObjectForKey(key) as NSString).ToArray();
+                }
+                else if (key == CBAdvertisement.DataManufacturerDataKey)
+                {
+                    var arr = ((NSData)advertisementData.ObjectForKey(key)).ToArray();
+                    advertisementRecord.ManufacturerSpecificData = arr;
+                }
+                else if (key == CBAdvertisement.DataServiceUUIDsKey)
+                {
+                    var array = (NSArray)advertisementData.ObjectForKey(key);
+
+                    var dataList = new List<NSData>();
+                    for (nuint i = 0; i < array.Count; i++)
+                    {
+                        var cbuuid = array.GetItem<CBUUID>(i);
+                        dataList.Add(cbuuid.Data);
+                    }
+                    advertisementRecord.UuidsComplete128Bit = dataList.SelectMany(d => d.ToArray()).ToArray();
+                }
+                else if (key == CBAdvertisement.DataTxPowerLevelKey)
+                {
+                    sbyte byteValue = Convert.ToSByte(((NSNumber)advertisementData.ObjectForKey(key)).Int32Value);
+                    byte[] arr = { (byte)byteValue };
+                    advertisementRecord.TxPowerLevel = arr;
+                }
+                else if (key == CBAdvertisement.DataServiceDataKey)
+                {
+                    NSDictionary serviceDict = (NSDictionary)advertisementData.ObjectForKey(key);
+                    foreach (CBUUID dKey in serviceDict.Keys)
+                    {
+                        byte[] keyAsData = dKey.Data.ToArray();
+
+                        Array.Reverse(keyAsData);
+
+                        var data = (NSData)serviceDict.ObjectForKey(dKey);
+                        byte[] valueAsData = data.Length > 0 ? data.ToArray() : new byte[0];
+
+                        byte[] arr = new byte[keyAsData.Length + valueAsData.Length];
+                        Buffer.BlockCopy(keyAsData, 0, arr, 0, keyAsData.Length);
+                        Buffer.BlockCopy(valueAsData, 0, arr, keyAsData.Length, valueAsData.Length);
+
+                        advertisementRecord.ServiceData = arr;
+                    }
+                }
+                else if (key == CBAdvertisement.IsConnectable)
+                {
+                    advertisementRecord.IsConnectable = new byte[] { ((NSNumber)advertisementData.ObjectForKey(key)).ByteValue };
+                }
+                else
+                {
+                    Console.WriteLine($"Parsing Advertisement: Not parsed {key.ToString()}");
+                }
+            }
+
+            return advertisementRecord;
+        }
+
+        // 2. Parse CBUUID to Guid
+        private Guid ParseDeviceGuid(CBPeripheral peripherial)
+        {
+            return Guid.ParseExact(peripherial.Identifier.AsString(), "d");
+        }
+        #endregion
+
+    }
+}
---
+++ ModifiedLibFiles/BluetoothPeripheralService.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoreBluetooth;
+using CoreFoundation;
+using EY.Mobile.Bluetooth.Enums;
+using EY.Mobile.Bluetooth.EventArgs;
+using Foundation;
+using System.Linq;
+
+namespace EY.Mobile.Bluetooth.iOS
+{
+    public class BluetoothPeripheralService : CBPeripheralManagerDelegate,IBluetoothPeripheralService
+    {
+        private static string _restorationIdentifier;
+
+        public static void UseRestorationIdentifier(string restorationIdentifier)
+        {
+            _restorationIdentifier = restorationIdentifier;
+        }
+
+        CBPeripheralManager peripheralManager;
+        IList<CBMutableService> servicesList;
+        IList<CBMutableCharacteristic> characteristicsList;
+
+        public BluetoothPeripheralService()
+        {
+            Console.WriteLine("iOS :: Concstructor of BluetoothPeripheralService invoked");
+        }
+
+        #region 1. Creation of Ble Peripheral Object
+
+        public Task CreatePeripheral()
+        {
+            peripheralManager = new CBPeripheralManager(this, DispatchQueue.CurrentQueue, InitOptions);
+            servicesList?.Clear();
+            characteristicsList?.Clear();
+            return Task.CompletedTask;
+        }
+
+        private NSDictionary InitOptions
+        {
+            get
+            {
+                var keys = new[] { new NSString(CBPeripheralManager.OptionRestoreIdentifierKey) };
+                var values = new[] { new NSString(_restorationIdentifier) };
+                var options = new NSDictionary<NSString, NSObject>(keys, values);
+                return options;
+            }
+        }
+
+        #endregion
+
+        #region Implementation of CBPeripheralManagerDelegate methods
+
+        // 1. Peripheral state is updated
+        private event EventHandler<BluetoothState> _bluetoothStateUpdated;
+        public event EventHandler<BluetoothState> BluetoothStateUpdated
+        {
+            add => _bluetoothStateUpdated += value;
+            remove => _bluetoothStateUpdated -= value;
+        }
+
+        public override void StateUpdated(CBPeripheralManager peripheral)
+        {
+            _bluetoothStateUpdated?.Invoke(this, peripheral.State.ToBluetoothState());
+        }
+
+        // 2. Peripheral started advertising
+        private event EventHandler<bool> _deviceAdvertised;
+        public event EventHandler<bool> DeviceAdvertised
+        {
+            add => _deviceAdvertised += value;
+            remove => _deviceAdvertised -= value;
+        }
+
+        private event EventHandler<BTAdvertiseFailureEventArgs> _deviceAdvertiseError;
+        public event EventHandler<BTAdvertiseFailureEventArgs> DeviceAdvertiseError
+        {
+            add => _deviceAdvertiseError += value;
+            remove => _deviceAdvertiseError -= value;
+        }
+
+        public override void AdvertisingStarted(CBPeripheralManager peripheral, NSError error)
+        {
+            if (error == null)
+            {
+                _deviceAdvertised?.Invoke(this, (peripheralManager != null) ? peripheralManager.Advertising : false);
+            }
+            else
+            {
+                _deviceAdvertiseError?.Invoke(this, new BTAdvertiseFailureEventArgs() { ErrorMessage = error.LocalizedDescription});
+            }
+        }
+
+        // 3. Peripheral state is restored by iOS syste, when app is already terminated.
+        private event EventHandler<BTPeripheralStateRestoredEventArgs> _peripheralStateRestored;
+        public event EventHandler<BTPeripheralStateRestoredEventArgs> PeripheralStateRestored
+        {
+            add => _peripheralStateRestored += value;
+            remove => _peripheralStateRestored -= value;
+        }
+
+        public override void WillRestoreState(CBPeripheralManager peripheral, NSDictionary dict)
+        {
+            var args = new BTPeripheralStateRestoredEventArgs();
+            args.IsAdvertising = peripheral.Advertising;
+
+            if (dict.ContainsKey(CBPeripheralManager.RestoredStateServicesKey))
+            {
+                var servicesRestored = (NSArray)dict[CBPeripheralManager.RestoredStateServicesKey];
+                if (servicesRestored != null && servicesRestored.Count > 0)
+                {
+                    var services = new LocalPeripheralService[servicesRestored.Count];
+                    for (nuint i = 0; i < servicesRestored.Count; i++)
+                    {
+                        var nativeService = servicesRestored.GetItem<CBMutableService>(i);
+                        services[i] = new LocalPeripheralService(nativeService);
+                    }
+                    args.RestoredServices = services;
+                }
+            }
+            _peripheralStateRestored?.Invoke(this, args);
+        }
+
+        // 4. Received Read Request
+        private event EventHandler<BTGattCharacteristicReadRequestEventArgs> _receivedReadRequest;
+        public event EventHandler<BTGattCharacteristicReadRequestEventArgs> ReceivedReadRequest
+        {
+            add => _receivedReadRequest += value;
+            remove => _receivedReadRequest -= value;
+        }
+        public override void ReadRequestReceived(CBPeripheralManager peripheral, CBATTRequest request)
+        {
+            var readRequest = new BTGattCharacteristicReadRequest();
+            readRequest.Central = request.Central;
+            readRequest.Characteristic = new LocalPeripheralCharacteristic((CBMutableCharacteristic)request.Characteristic);
+            readRequest.Offset = (int)request.Offset;
+            readRequest.Value = request.Value.ToArray();
+            readRequest.NativeRequest = request;
+            _receivedReadRequest?.Invoke(this, new BTGattCharacteristicReadRequestEventArgs() { ReadRequest = readRequest });
+        }
+
+        // 5. Received Write Request
+        private event EventHandler<BTGattCharacteristicWriteRequestEventArgs> _receivedWriteRequest;
+        public event EventHandler<BTGattCharacteristicWriteRequestEventArgs> ReceivedWriteRequest
+        {
+            add => _receivedWriteRequest += value;
+            remove => _receivedWriteRequest -= value;
+        }
+        public override void WriteRequestsReceived(CBPeripheralManager peripheral, CBATTRequest[] requests)
+        {
+            var writeRequest = new BTGattCharacteristicWriteRequest();
+            writeRequest.Central = requests[0].Central;
+            writeRequest.Characteristic = new LocalPeripheralCharacteristic((CBMutableCharacteristic)requests[0].Characteristic);
+            writeRequest.Offset = (int)requests[0].Offset;
+            writeRequest.Value = requests[0].Value.ToArray();
+            writeRequest.NativeRequest = requests[0];
+            _receivedWriteRequest?.Invoke(this, new BTGattCharacteristicWriteRequestEventArgs() { WriteRequest = writeRequest });
+        }
+
+        #endregion
+
+        #region Create/Add/Remove Services
+
+        public ILocalPeripheralService CreateLocalPeripheralService(Guid guid, bool isPrimary, ILocalPeripheralCharacteristic[] characteristics)
+        {
+            return new LocalPeripheralService(guid, isPrimary, characteristics);
+        }
+
+        public ILocalPeripheralCharacteristic CreateLocalPeripheralCharacteristic(Guid guid, CharacteristicPropertyType properties, CharacteristicPermissionType permissions, byte[] value)
+        {
+            return new LocalPeripheralCharacteristic(guid, properties, permissions, value);
+        }
+
+        public bool AddService(ILocalPeripheralService service)
+        {
+            if(servicesList == null)
+            {
+                servicesList = new List<CBMutableService>();
+            }
+
+            if (characteristicsList == null)
+            {
+                characteristicsList = new List<CBMutableCharacteristic>();
+            }
+
+            foreach (var characteristic in service.Characteristics)
+            {
+                characteristicsList.Add((CBMutableCharacteristic)characteristic.NativeCharacteristic);
+            }
+
+            servicesList.Add((CBMutableService)service.NativeService);
+
+            peripheralManager.AddService((CBMutableService)service.NativeService);
+            return true;
+        }
+
+        public void RemoveService(ILocalPeripheralService service)
+        {
+            peripheralManager?.RemoveService((CBMutableService)service.NativeService);
+        }
+
+        public void RemoveAllServices()
+        {
+            peripheralManager?.RemoveAllServices();
+        }
+
+        #endregion
+
+        #region Current state of the Peripheral
+
+        public BluetoothState GetPeripheralState()
+        {
+            return peripheralManager.State.ToBluetoothState();
+        }
+
+        #endregion
+
+
+        #region Send notification to subscribed centrals
+
+        public bool SendNotification(ILocalPeripheralCharacteristic characteristic, byte[] value, bool confirm)
+        {
+            var characteristicsToBeUpdated = characteristicsList.FirstOrDefault(x => x.UUID == characteristic.Guid.cbuuidFromGuid());
+            bool result = false;
+            if (characteristicsToBeUpdated != null)
+            {
+                result = peripheralManager.UpdateValue(NSData.FromArray(value), characteristicsToBeUpdated, null);
+            }
+            return result; // TODO: Define proper response which includes meaning information about failure.
+        }
+
+        #endregion
+
+        #region Responding to Read/Write request
+        
+        public void RespondToReadRequest(BTGattCharacteristicReadResponse response)
+        {
+            peripheralManager.RespondToRequest((CBATTRequest)response.ReadRequest.NativeRequest, response.Result.ToNative());
+        }
+
+        public void RespondToWriteRequest(BTGattCharacteristicWriteResponse response)
+        {
+            peripheralManager.RespondToRequest((CBATTRequest)response.WriteRequest.NativeRequest, response.Result.ToNative());
+        }
+
+        #endregion
+
+        #region Advertising
+
+        public Task StartAdvertising(BTAdvertiseData data)
+        {
+            CBUUID[] servicesUuid = Array.ConvertAll(data.ServiceGuids,
+            new Converter<Guid, CBUUID>(GuidToCbuuid));
+            //var serviceUuids = new CBUUID[data.ServiceGuids.Length];
+            //for (int i = 0; i < data.ServiceGuids.Length; i++)
+            //{
+            //    serviceUuids[i] = data.ServiceGuids[i].cbuuidFromGuid();
+            //}
+            var options = new StartAdvertisingOptions() { LocalName = data.DeviceName, ServicesUUID = servicesUuid };
+            peripheralManager?.StartAdvertising(options);
+            return Task.CompletedTask;
+        }
+
+        private CBUUID GuidToCbuuid(Guid guid)
+        {
+            return guid.cbuuidFromGuid();
+        }
+
+        public Task StopAdvertising()
+        {
+            peripheralManager?.StopAdvertising();
+            return Task.CompletedTask;
+        }
+
+        public bool IsAdvertising()
+        {
+            return (peripheralManager != null)? peripheralManager.Advertising : false;
+        }
+
+        #endregion
+
+    }
+}
